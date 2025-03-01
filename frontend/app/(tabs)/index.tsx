be67ab5ccd5448da28263821b0324c9c9dfbb7f6
Jasnoor Saini <jasnoor.saini@icloud.com>
2025-03-01 17:58:57 -0500

    downloaded dependencies and created hook

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,56 +1,36 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Alert, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { usePermissions } from 'expo-media-library';
+import { useCameraPermissions } from 'expo-camera';
 
 export default function HomeScreen() {
+  const [cameraPermissions, requestCameraPermissions] = useCameraPermissions();
+  const [mediaLibraryPermissions, requestMediaLibraryPermissions] = usePermissions();
+
+  async function requestAllPermissions() {
+    const { status } = await requestCameraPermissions();
+    if (status != 'granted') {
+      Alert.alert('Permission required', 'Please allow camera permissions to use this feature.');
+    }
+
+    const { status: mediaLibraryStatus } = await requestMediaLibraryPermissions();
+    if (mediaLibraryStatus != 'granted') {
+      Alert.alert('Permission required', 'Please allow media library permissions to use this feature.');
+    }
+  }
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ThemedView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+
+      <Button title="Request Camera Permissions" onPress={requestPermissions} />
+    </ThemedView>
+      
   );
 }
 
