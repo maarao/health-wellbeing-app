277efce438e7490cce87a1e1be56dfe5649a2008
Jasnoor Saini <jasnoor.saini@icloud.com>
2025-03-02 04:24:31 -0500

    fixed small warning

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -41,7 +41,7 @@ export default function TabLayout() {
         }
       }}>
       <Tabs.Screen
-        name="settings"
+        name="profile"
         options={{
           title: 'Profile',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="person" color={color} />,
